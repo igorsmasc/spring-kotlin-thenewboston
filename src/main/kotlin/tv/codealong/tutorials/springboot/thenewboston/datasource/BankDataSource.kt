1a235787fb70107d6039cd4c2f4e51a020fd8a89
“Your <igorsmascarenhas@outlook.com>
2021-07-21 17:06:26 -0300

    data source

---
+++ src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/datasource/BankDataSource.kt
@@ -0,0 +1,9 @@
+package tv.codealong.tutorials.springboot.thenewboston.datasource
+
+import tv.codealong.tutorials.springboot.thenewboston.model.Bank
+
+interface BankDataSource {
+
+    fun retrieveBanks(): Collection<Bank>
+
+}
---
+++ src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/datasource/mock/MockBankDataSource.kt
@@ -0,0 +1,22 @@
+package tv.codealong.tutorials.springboot.thenewboston.datasource.mock
+
+import org.springframework.stereotype.Repository
+import tv.codealong.tutorials.springboot.thenewboston.datasource.BankDataSource
+import tv.codealong.tutorials.springboot.thenewboston.model.Bank
+
+@Repository
+class MockBankDataSource : BankDataSource{
+
+    val banks = listOf(
+        Bank("123",
+            1.0, 10),
+        Bank("123",
+            1.0, 0),
+        Bank("123",
+            0.0, 10)
+    )
+
+
+    override fun retrieveBanks(): Collection<Bank> = banks
+
+}
