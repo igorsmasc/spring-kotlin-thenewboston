8692ed6ebadc45cb489cd0445193f19228dfefc3
“Your <igorsmascarenhas@outlook.com>
2021-07-22 00:54:21 -0300

    web layer

---
+++ src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/controller/BankController.kt
@@ -0,0 +1,4 @@
+package tv.codealong.tutorials.springboot.thenewboston.controller
+
+class BankController {
+}
---
+++ src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/controller/BankControllerTest.kt
@@ -0,0 +1,5 @@
+package tv.codealong.tutorials.springboot.thenewboston.controller
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BankControllerTest
